59b9d568baab3fce93166a4c379985d921d7a901
Marcel Tanuri <marcel.tanuri@gmail.com>
2023-08-22 10:09:31 -0300

    initial commit

---
+++ src/main/java/br/com/mtanuri/ada/t1042/web2/projeto/AdaTurma1042Web2Application.java
@@ -0,0 +1,13 @@
+package br.com.mtanuri.ada.t1042.web2.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaTurma1042Web2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaTurma1042Web2Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mtanuri/ada/t1042/web2/projeto/AdaTurma1042Web2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mtanuri.ada.t1042.web2.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaTurma1042Web2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
