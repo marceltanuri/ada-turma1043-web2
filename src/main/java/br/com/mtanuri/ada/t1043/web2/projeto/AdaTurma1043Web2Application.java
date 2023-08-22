2ecc69094b94b367aa637939795252c089f7b713
Marcel Tanuri <marcel.tanuri@gmail.com>
2023-08-22 18:53:58 -0300

    rename project

--- src/main/java/br/com/mtanuri/ada/t1043/web2/projeto/AdaTurma1043Web2Application.java
+++ src/main/java/br/com/mtanuri/ada/t1043/web2/projeto/AdaTurma1043Web2Application.java
@@ -1,13 +1,13 @@
-package br.com.mtanuri.ada.t1042.web2.projeto;
+package br.com.mtanuri.ada.t1043.web2.projeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdaTurma1042Web2Application {
+public class AdaTurma1043Web2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdaTurma1042Web2Application.class, args);
+		SpringApplication.run(AdaTurma1043Web2Application.class, args);
 	}
 
 }
--- src/test/java/br/com/mtanuri/ada/t1043/web2/projeto/AdaTurma1043Web2ApplicationTests.java
+++ src/test/java/br/com/mtanuri/ada/t1043/web2/projeto/AdaTurma1043Web2ApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.mtanuri.ada.t1042.web2.projeto;
+package br.com.mtanuri.ada.t1043.web2.projeto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AdaTurma1042Web2ApplicationTests {
+class AdaTurma1043Web2ApplicationTests {
 
     @Test
     void contextLoads() {
