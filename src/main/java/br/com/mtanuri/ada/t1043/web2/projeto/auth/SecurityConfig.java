b40d8c8c43bd53f80578412b6cf73b747b15f73b
LucasMSF <lucasgg003@gmail.com>
2023-08-26 13:34:39 -0300

    Spring security com Basic auth
    Autorização no swagger

---
+++ src/main/java/br/com/mtanuri/ada/t1043/web2/projeto/auth/SecurityConfig.java
@@ -0,0 +1,30 @@
+package br.com.mtanuri.ada.t1043.web2.projeto.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final String[] publicPaths = {
+            "/swagger-ui.html/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    };
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.authorizeHttpRequests((authorize) -> authorize.requestMatchers(publicPaths)
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+
+    }
+}
